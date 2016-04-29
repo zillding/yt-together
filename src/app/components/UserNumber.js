8cbff02a9b6aa3fb1aa0b695065200bb4bed2d25
Zill Ding <zilldzy@gmail.com>
2016-04-29 23:11:35 +0800

    Add user number label animation

--- src/app/components/UserNumber.js
+++ src/app/components/UserNumber.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import { generateNonDupInt } from '../utils'
+
 const style = {
   marginLeft: '1em',
 }
 
 const UserNumber = ({ number }) => (
   <div
-    className="ui large red label"
+    className={`ui large red label ${getClassName()}`}
     style={style}>
     <i className="users icon"></i>
     {number}
@@ -14,3 +16,21 @@ const UserNumber = ({ number }) => (
 )
 
 export default UserNumber
+
+function getClassName() {
+  // generate a random non duplicated
+  // animate class name
+  const classArray = [
+    'bounce',
+    'flash',
+    'pulse',
+    'rubberBand',
+    'shake',
+    'swing',
+    'tada',
+    'wobble',
+    'jello',
+  ]
+  const index = generateNonDupInt(0, classArray.length - 1)
+  return `animated ${classArray[index]}`
+}
--- src/app/utils.js
+++ src/app/utils.js
@@ -1,3 +1,6 @@
+import isNull from 'lodash/isNull'
+import random from 'lodash/random'
+
 export function getVideoIndex(playlist, videoId) {
   return playlist.findIndex(video => {
     return video.id.videoId === videoId
@@ -31,4 +34,19 @@ export function getPreviousVideoId(playlist, currentVideoId) {
   const index = getVideoIndex(playlist, currentVideoId) - 1
   if (index < 0) return ''
   return playlist.get(index).id.videoId
+}
+
+let previousNum = null
+export function generateNonDupInt(low, high) {
+  const result = random(low, high)
+
+  // first time generate
+  if (isNull(previousNum)) return result
+
+  if (result !== previousNum) {
+    previousNum = result
+    return result
+  }
+
+  return generateNonDupInt(low, high)
 }
